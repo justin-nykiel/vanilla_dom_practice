e7419bc8830d5cb4d408c9a0722ccd84eb96e606
justin-nykiel <justin.nykiel@gmail.com>
2020-09-07 09:36:56 -0500

    selected elements by class and console logged the node list

--- dom_practice.js
+++ dom_practice.js
@@ -4,3 +4,6 @@ console.log(marltonArticle);
 const h2 = document.querySelector("h2");
 console.log(h2);
 h2.innerText += "!!!";
+
+const headlines = document.querySelectorAll(".headlines");
+console.log(headlines);
