d3f0b26cda04b4be1d362b014dd137ab9fe71004
justin-nykiel <justin.nykiel@gmail.com>
2020-09-07 09:56:00 -0500

    appended a img element to the DOM

--- dom_practice.js
+++ dom_practice.js
@@ -11,3 +11,6 @@ const liOne = document.querySelector("ol").firstElementChild;
 liOne.style.color = "slateblue";
 const liLast = document.querySelector("ol").lastElementChild;
 liLast.innerText = "Jamboree sandwich at the Jambo-ree";
+const newImg = document.createElement('img');
+newImg.setAttribute("src", "https://lh3.googleusercontent.com/proxy/seyOs6sf0lh8lXDf_OQFgHnqO2Z5A47zw_BrqBccyiX7uR7CncDaf3lF4nN-gcJMUBijFR7zBOpJsS2h6MLiSaw51afwWOQi_ViuhaOJ6wzSjUusUWi5idc0X-7Mrv4tFwfGhED9v7dxZQRsvhdoqT_TVk0");
+document.querySelector("body").appendChild(newImg);
